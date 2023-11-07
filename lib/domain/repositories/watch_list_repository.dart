19d8dd2738936b5f805a0cf262db3b0b7d85f43d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-07 20:23:42 +0700

    add watch list repository

---
+++ lib/domain/repositories/watch_list_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+
+// abstract class WatchlistRepository {
+//   Future<Either<Failure, String>> saveWatchlist(ContentData contentData);
+//   Future<Either<Failure, String>> removeWatchlist(int id, int dataType);
+//   Future<bool> isAddedToWatchlist(int id, int dataType);
+//   Future<Either<Failure, List<IdPosterTitleOverview>>> getWatchlist();
+// }
